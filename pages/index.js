d39cd703ad4968e53dc3eb54c205d6ff1584e27c
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-02-04 09:58:57 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import { baseUrl, fetchApi } from '../utils/fetchApi'
+import ArtistSearch from './search'
+
+
+export default function Home() {
+
+
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Brain</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        {/* <ArtistSearch/> */}
+      </main>
+
+      
+    </div>
+  )
+}
+
+
